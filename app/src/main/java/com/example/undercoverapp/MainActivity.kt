3e1859806fecd921e5e5801f0a1ef320ac4a1534
ccale <sdepeille@gmail.com>
2024-04-07 12:23:50 +0200

    added text box for the first person to not be mr white added ability for voted our mr white to guess the word

--- app/src/main/java/com/example/undercoverapp/MainActivity.kt
+++ app/src/main/java/com/example/undercoverapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.undercoverapp
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
@@ -42,7 +41,7 @@ import androidx.core.content.edit
 import com.example.undercoverapp.ui.theme.UndercoverAppTheme
 
 const val INNOCENT = "Innocent"
-const val IMPOSTER = "Imposteur"
+const val UNDERCOVER = "Imposteur"
 const val MRWHITE = "Mister White"
 const val DEFAULTWORDS = "Chat, Chien\n" + "Café, Thé\n" + "Pluie, Soleil\n" + "Livre, Film\n" + "Pizza, Sushi\n" + "Mer, Montagne\n" + "Vélo, Voiture\n" + "Piano, Guitare\n" + "Été, Hiver\n" + "Téléphone, Ordinateur"
 
@@ -59,11 +58,12 @@ class MainActivity : ComponentActivity() {
 
                 val defaultWordPair = pairPicker(wordsList)
                 var innocentWord by remember { mutableStateOf(defaultWordPair.first) }
-                var imposterWord by remember { mutableStateOf(defaultWordPair.second) }
+                var undercoverWord by remember { mutableStateOf(defaultWordPair.second) }
 
                 var innocentCount by remember { mutableIntStateOf(0) }
-                var imposterCount by remember { mutableIntStateOf(0) }
+                var undercoverCount by remember { mutableIntStateOf(0) }
                 var mrWhiteCount by remember { mutableIntStateOf(0) }
+                var showFirstPerson by remember { mutableStateOf(false) }
 
 
                 fun startGame(){
@@ -76,18 +76,38 @@ class MainActivity : ComponentActivity() {
                         person.isInGame = true
                         person.role = INNOCENT
                     }
-                    for (person in tempPeople.subList(innocentCount, innocentCount + imposterCount)){
+                    for (person in tempPeople.subList(innocentCount, innocentCount + undercoverCount)){
                         person.isInGame = true
-                        person.role = IMPOSTER
+                        person.role = UNDERCOVER
                     }
-                    for (person in tempPeople.subList(innocentCount + imposterCount, tempPeople.size)){
+                    for (person in tempPeople.subList(innocentCount + undercoverCount, tempPeople.size)){
                         person.isInGame = true
                         person.role = MRWHITE
                     }
                     val wordPair = pairPicker(wordsList)
                     innocentWord = wordPair.first
-                    imposterWord = wordPair.second
+                    undercoverWord = wordPair.second
                     people = people - fernano
+
+                    showFirstPerson = true
+
+                }
+
+                if (showFirstPerson){
+                    Dialog(onDismissRequest = { showFirstPerson = false }) {
+                        Card(modifier = Modifier
+                            .fillMaxSize(0.9f)){
+
+                            val tempPeople = people.shuffled()
+                            var firstPersonText = "Il n'y a que des Mr White... ???"
+                            tempPeople.forEach {
+                                if (it.role != MRWHITE){
+                                    firstPersonText = "${it.name} commence en premier"
+                                }
+                            }
+                            Text(text = firstPersonText)
+                        }
+                    }
                 }
 
                 Column(
@@ -148,7 +168,6 @@ class MainActivity : ComponentActivity() {
                                         .align(Alignment.CenterVertically)
                                         .weight(1f)
                                 )
-                                Log.d("in game?", "${person.name} is in game: $isInGame")
 
                                 if (isInGame){
                                     Button(onClick = { showWord = true }) {
@@ -162,7 +181,7 @@ class MainActivity : ComponentActivity() {
                                             Text(
                                                 text = when (person.role) {
                                                     INNOCENT -> innocentWord
-                                                    IMPOSTER -> imposterWord
+                                                    UNDERCOVER -> undercoverWord
                                                     else -> "Pas de mot!" },
                                                 modifier = Modifier
                                                     .align(Alignment.CenterHorizontally)
@@ -179,15 +198,50 @@ class MainActivity : ComponentActivity() {
                                 if (showEject){
                                     person.isInGame = false
                                     Dialog(onDismissRequest = { showEject = false }) {
-                                        Card(modifier = Modifier.fillMaxWidth(0.9f)) {
-                                            Text(text = when (person.role) {
-                                                INNOCENT -> "${person.name} était innocent!"
-                                                IMPOSTER -> "${person.name} était un imposteur!"
-                                                else -> "${person.name} était mister white!"},
-                                            modifier = Modifier
-                                                .align(Alignment.CenterHorizontally)
-                                                .fillMaxSize(0.8f)
-                                            )
+                                        Card(modifier = Modifier.fillMaxSize(0.9f)) {
+                                            Column {
+                                                Text(
+                                                    text = when (person.role) {
+                                                        INNOCENT -> "${person.name} était innocent!"
+                                                        UNDERCOVER -> "${person.name} était undercover!"
+                                                        else -> "${person.name} était mister white!"
+                                                    },
+                                                    modifier = Modifier
+                                                        .align(Alignment.CenterHorizontally)
+                                                )
+                                                if (person.role == MRWHITE){
+                                                    var isRightWord by remember { mutableStateOf(false) }
+                                                    var isButtonBeenClicked by remember { mutableStateOf(false)}
+                                                    var textField by remember { mutableStateOf("") }
+                                                    Column(modifier = Modifier
+                                                        .padding(8.dp)
+                                                    ) {
+                                                        OutlinedTextField(
+                                                            value = textField,
+                                                            onValueChange = { text -> textField = text},
+                                                            modifier = Modifier
+                                                                .fillMaxWidth(0.6f)
+                                                                .align(Alignment.CenterHorizontally)
+                                                        )
+                                                        Button(
+                                                            onClick = { if (textField.lowercase().trim() == innocentWord.lowercase().trim()){ isRightWord = true } ; isButtonBeenClicked = true },
+                                                            modifier = Modifier
+                                                                .fillMaxWidth(0.4f)
+                                                                .align(Alignment.CenterHorizontally)
+                                                        ) {
+                                                            Text(text = "Deviner le mot")
+                                                        }
+                                                        if (isButtonBeenClicked){
+                                                            var answerText = "Ce n'est pas le bon mot !"
+                                                            if (isRightWord) { answerText = "${person.name} a trouvé le bon mot et a gagné !"}
+                                                            Text(text = answerText,
+                                                                modifier = Modifier
+                                                                    .align(Alignment.CenterHorizontally)
+                                                            )
+                                                        }
+                                                    }
+                                                }
+                                            }
                                         }
                                     }
                                 }
@@ -283,23 +337,23 @@ class MainActivity : ComponentActivity() {
                                         .padding(8.dp)) {
                                         val maxValue = people.size
                                         var tempInnocent by remember { mutableIntStateOf(0) }
-                                        var tempImposter  by remember { mutableIntStateOf(0) }
+                                        var tempUndercover  by remember { mutableIntStateOf(0) }
                                         var tempMrWhite by remember { mutableIntStateOf(0) }
 
-                                        var enoughPeople by remember { mutableStateOf(tempInnocent + tempMrWhite + tempImposter == maxValue)}
+                                        var enoughPeople by remember { mutableStateOf(tempInnocent + tempMrWhite + tempUndercover == maxValue)}
 
                                         fun isEnoughPeople(a: Int, b: Int, c: Int, d:Int): Boolean{
                                             return (a + b + c == d)
                                         }
 
                                         Text(text = "$maxValue joueurs")
-                                        NumberPicker("Innocent", maxValue ) { number -> tempInnocent = number ; enoughPeople = isEnoughPeople(tempInnocent, tempImposter, tempMrWhite, maxValue) }
-                                        NumberPicker("Imposteurs", maxValue ) { number -> tempImposter = number ; enoughPeople = isEnoughPeople(tempInnocent, tempImposter, tempMrWhite, maxValue) }
-                                        NumberPicker("Mr White", maxValue ) { number -> tempMrWhite = number ; enoughPeople = isEnoughPeople(tempInnocent, tempImposter, tempMrWhite, maxValue)}
+                                        NumberPicker("Innocent", maxValue ) { number -> tempInnocent = number ; enoughPeople = isEnoughPeople(tempInnocent, tempUndercover, tempMrWhite, maxValue) }
+                                        NumberPicker("Imposteurs", maxValue ) { number -> tempUndercover = number ; enoughPeople = isEnoughPeople(tempInnocent, tempUndercover, tempMrWhite, maxValue) }
+                                        NumberPicker("Mr White", maxValue ) { number -> tempMrWhite = number ; enoughPeople = isEnoughPeople(tempInnocent, tempUndercover, tempMrWhite, maxValue)}
                                         
                                         Spacer(modifier = Modifier.size(8.dp))
                                         
-                                        Button(onClick = { innocentCount = tempInnocent ; imposterCount = tempImposter ; mrWhiteCount = tempMrWhite ; startGame() ; showDialogStartGame = false },
+                                        Button(onClick = { innocentCount = tempInnocent ; undercoverCount = tempUndercover ; mrWhiteCount = tempMrWhite ;  showDialogStartGame = false ; startGame()},
                                             enabled = enoughPeople) {
                                             Text(text = if (enoughPeople) "Lancer la partie" else "Mauvais nombre de joueurs")
                                         }
@@ -307,6 +361,8 @@ class MainActivity : ComponentActivity() {
                                 }
                             }
                         }
+
+
                     }
                 }
             }
